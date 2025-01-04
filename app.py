65cb72e0ae63377c7f68b0923283094fdc535183
ashrf <139250377+Ashrfalkhtry1@users.noreply.github.com>
2025-01-04 17:20:20 +0300

    app.py

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'dev_ashrf'
 
 
 if __name__ == "__main__":
     app.run()
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
